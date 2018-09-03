c58b87f73b5b1ea029ffc346d06c6e787a6d0fa8
gregcargo <greg@react-digital.com>
2018-09-03 07:36:33 +0400

    added room list component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDPgQlyFlyKmUdh0KRCOUribfy1EBs0uv8",
+    authDomain: "bloc-chat-react-71abc.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-71abc.firebaseio.com",
+    projectId: "bloc-chat-react-71abc",
+    storageBucket: "bloc-chat-react-71abc.appspot.com",
+    messagingSenderId: "613486536320"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
