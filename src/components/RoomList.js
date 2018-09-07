92744b53eb7e27d38028691568de24431731d26a
gregcargo <greg@react-digital.com>
2018-09-08 01:18:42 +0400

    checkpoint 3 started

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -10,22 +10,41 @@ class RoomList extends Component {
 		this.roomsRef = this.props.firebase.database().ref('rooms');
 		};
 
-		render() {
-			return this.state.rooms.map ( (rooms) =>
-				<section className="RoomList" key={rooms.key}>
-    				{rooms.name}
-  				</section>
-  			);
-		}
+	componentDidMount() {
+     	this.roomsRef.on('child_added', snapshot => {
+     		const room = snapshot.val();
+       		room.key = snapshot.key;
+       
+       	this.setState({ rooms: this.state.rooms.concat( room ) })
+     	});
+   		}
 
-componentDidMount() {
-     this.roomsRef.on('child_added', snapshot => {
-     	const room = snapshot.val();
-       room.key = snapshot.key;
-       this.setState({ rooms: this.state.rooms.concat( room ) })
-     });
-   }
-}
+   	createRoom () {
+   		const newRoomName = this.state.rooms.name();
+   		this.roomsRef.push({
+  		name: newRoomName
+		});
+   		}
+
+	render() {
+		return (
+			<div>
+				<div>
+					{this.state.rooms.map ( (rooms) =>
+						<section className="RoomList" key={rooms.key}>
+    					{rooms.name}
+  						</section>
+
+  					)}
+  				</div>
+  				<form onSubmit={this.createRoom}>
+  					<label>Name:<input type="text" name="name" /></label>
+  					<input type="submit" value="Submit" />
+				</form>
+			</div>	
+  			)}
+	
+	}
 
 
 
